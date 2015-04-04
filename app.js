280a6eacab5eb766f52bdb6cf97fd291fcfb90b8
resilience_me <emailsareobsolete@gmail.com>
2015-04-04 02:28:45 +0000

    a

--- app.js
+++ app.js
@@ -1,31 +1,4 @@
-var express        =        require("express");
-var bodyParser     =        require("body-parser");
-var app            =        express();
 
-app.use(bodyParser.json());
-
-
-
-app.post('/bitcoin', function (req, res) {
-  if (req.body.payload.type == 'address') {
-    console.log('address=' + req.body.payload.address + ' ' +
-      'transaction=' + req.body.payload.transaction_hash + ' ' +
-      'sent=' + req.body.payload.sent + ' ' +
-      'received=' + req.body.payload.received + ' ' +
-      'confirmations=' + req.body.payload.confirmations);
-    res.send('OK\n');
-  }
-});
-
-
-app.get('/', function(req, res) {
-  res.type('text/plain');
-  res.send('i am a beautiful butterfly');
-});
-
-app.listen(process.env.PORT || 4730,function(){
-  console.log("Started on PORT 4730");
-})
 
 
 
@@ -49,7 +22,7 @@ var connect_to_ripple = require('./financial_platforms/connect_to_ripple.js')
 connect_to_ripple.connect(db_ripple)
 
 var connect_to_bitcoin = require('./financial_platforms/connect_to_bitcoin.js')
-connect_to_ripple.connect(db_bitcoin)
+connect_to_bitcoin.connect(db_bitcoin)
 
 
 
--- financial_platforms/connect_to_bitcoin.js
+++ financial_platforms/connect_to_bitcoin.js
@@ -1,170 +1,35 @@
 exports.connect = function(db_bitcoin){
     
     
+var express        =        require("express");
+var bodyParser     =        require("body-parser");
+var app            =        express();
+
+app.use(bodyParser.json());
+
+
+
+app.post('/bitcoin', function (req, res) {
+  if (req.body.payload.type == 'address') {
+    console.log('address=' + req.body.payload.address + ' ' +
+      'transaction=' + req.body.payload.transaction_hash + ' ' +
+      'sent=' + req.body.payload.sent + ' ' +
+      'received=' + req.body.payload.received + ' ' +
+      'confirmations=' + req.body.payload.confirmations);
+    res.send('OK\n');
     
-    
-    
-    
-
-// ----- UNFINISHED BITCOIN SCRIPT -----
-// add your financial platform here, here's example code for Bitcoin
-
-// someone else can finish this, I like ripple more
-
-// https://www.coinhook.co/docs, https://chain.com/notifications/setup
-    
-
-
-var WebSocket = require('ws');
-var ws = new WebSocket('wss://ws.blockchain.info/inv');
-
-
-ws.on('open', function open() {
-  ws.send({"op":"addr_sub", "addr":"1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp"})
-});
- 
-ws.on('message', function(data, flags) {
-    console.log(data)
-    console.log(flags)
-  // flags.binary will be set if a binary data is received. 
-  // flags.masked will be set if the data was masked. 
+  }
 });
 
 
+app.get('/', function(req, res) {
+  res.type('text/plain');
+  res.send('i am a beautiful butterfly');
+});
 
-var blocktrail = require('blocktrail-sdk');
- 
-console.log("123456789 Satoshi to BTC: ", blocktrail.toBTC(123456789));
-console.log("1.23456789 BTC to Satoshi: ", blocktrail.toSatoshi(1.23456789));
-
-
-var client = blocktrail.BlocktrailSDK({apiKey: "724d383f0403bcdc77994332907acf75ff22f635", apiSecret: "52cc8d906c1b37f425fec97e1345a531f7be7037", network: "BTC", testnet: false});
-
-client.setupWebhook('https://user:pass@example.com/',
-    function(err, result) {/*...*/});
-    
-
-
-
-
-
-
-
-// load all connected users from that financial platform
-function get_all_collections(){
-    
-var get_all_collections =require('../get_all_collections.js')
-get_all_collections.get_all_collections(function(accounts){
-                request_subscribe(accounts)
-
-})
-}
-get_all_collections()
-
-
-function request_subsribe(accounts){
-    
-    // someone fix this and connect Bitcoin like I've connected Ripple
-for(var i=0;i<accounts.length;i++){
-    client.subscribeAddressTransactions(accounts[i], 6, function(err, result) {console.log(result)});
+app.listen(process.env.PORT || 4730,function(){
+  console.log("Started on PORT 4730");
+})    
     
     
-}
-
-blablabla.on('transaction', function(data){
-
-
- 
-
-// filter out payement transactions
-
-
-if(data.transaction.TransactionType === 'Payment'){
-
-// filter out transactions from accounts within the network    
-if(accounts.indexOf(data.transaction.Account)!== -1){
-console.log(data.transaction.TransactionType + "scanned")
-
-// filter by currencies that are in use by the account that recieved the payment
-
-  db.collection(data.transaction.Destination).find({type: "contract"}, function(err,doc){
-            console.log("hi")
-            for(var i=0;i<doc.length;i++){
-                
-                if(data.transaction.Amount.currency === doc[i].currency){
-                    
-                console.log(data.transaction)
-                console.log(data.engine_result_message)
-                    
-                    transaction()
-                break
-                
-                }
-                else consumption_outside_network()
-            }
-            
-            
-            
-            
-    }) 
-    
-    
-}
-else{
-    console.log(data.transaction.Account)
-    console.log(accounts.indexOf(data.transaction.Account))
-    console.log("transaction from an account outside the network")    
-}
-}
-
-// update consumption outside network penalty
-
-function consumption_outside_network(){
-    
-    // upsert extra-network consumption
-    db.collection(data.transaction.Account).findAndModify({
-        query: {type: "consumption_outside_network", currency: data.transaction.Amount.currency}, 
-        update:{$inc:{total_amount:Number(data.transaction.Amount.value)}}, 
-        upsert: true,
-        new: true
-        
-    }, 
-        function(err,doc){
-            console.log(doc)
-        })    
-    
-}
-
-
-
-// connect the transaction by creating a dividend pathway
-
-
-function transaction(){
-        
-    //get taxRate
-     db.collection(data.transaction.Destination).findOne({type: "contract", currency: data.transaction.Amount.currency}, function(err,doc){
-            var taxRate;
-
-
-            taxRate = doc.taxRate
-            console.log(taxRate)
-            console.log("taxRate" + taxRate)
-            
-            var connect_transaction = require('../connect_transaction.js')
-
-            // connect the transaction to the resilience network
-            connect_transaction.connect_transaction(data.transaction.Account, data.transaction.Destination, data.transaction.Amount.currency, data.transaction.Amount.value, taxRate)
-            
-            
-
-    }) 
-   
-} 
-         
-})
-    
-}
-
-
 }
